8c423b0483a4b5e2b5136753f663fd1519720158
alfredt6 <alfred.salvacion@gmail.com>
2020-06-16 06:23:00 +0800

    Added header component and collection item

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage.component';
 import Shop from './pages/shop/Shop.component';
+import Header from './components/header/Header.component.jsx'
 
 
 const App = () => {  
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
--- src/components/preview-collection/CollectionPreview.component.jsx
+++ src/components/preview-collection/CollectionPreview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../preview-collection/collectionpreview.styles.scss';
+import './collectionpreview.styles.scss';
+import CollectionItem from '../collection-item/CollectionItem.component';
 
 const CollectionPreview = ({title, items}) => {
     //console.log({otherCollectionProps});
@@ -7,11 +8,13 @@ const CollectionPreview = ({title, items}) => {
     return (
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            {items
-                .filter((item, idx) => idx < 4)
-                .map(item => (
-                <div key={item.id}>{item.name}</div>
-            ))}
+            <div className='preview'>
+                {items
+                    .filter((item, idx) => idx < 4)
+                    .map(({ id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />                
+                ))}
+            </div>            
         </div>
     )
 }
